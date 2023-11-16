ed95cdb99c7cda188e4d7afbe7de034b4b9d2914
Bjornanger <143402834+Bjornanger@users.noreply.github.com>
2023-11-16 15:32:09 +0100

    Update MikaelIntro.sql

--- MikaelIntro.sql
+++ MikaelIntro.sql
@@ -1,31 +1,6 @@
 
---CREATE DATABASE DjurDataBase;
-
---USE DjurDataBase
-
---CREATE Table DjurTbl ( 
-
---"ID" int PRIMARY KEY,
-
---"AnimalName" nvarchar(max) not null
-
---);
---INSERT INTO DjurTbl
---VALUES (01,'Hund'), (02,'Katt'), (03,'Kanin'), (04,'Hamster');
-
---SELECT * FROM DjurTbl;
-
---SELECT * FROM DjurTbl WHERE AnimalName LIKE '%a%';
---DROP TABLE Djur_Tbl;
---DROP TABLE DjurTbl;
---DROP DATABASE DjurDataBase;
-
-DROP DATABASE DjurDB;
-
-
-
 --Create DATABASE KlassenDB;
-USE KlassenDB
+--USE KlassenDB
 
 --CREATE TABLE GruppenTbl (
 --Id int PRIMARY KEY,
@@ -35,16 +10,16 @@ USE KlassenDB
 --Hemort nvarchar(max) not null
 --);
 
-INSERT INTO GruppenTbl VALUES
- (1, 'Tyr', 'Hvidfeldt', 37, 'Göteborg')
- INSERT INTO GruppenTbl VALUES 
- (2, 'Gabriel', 'Raimondo', 31, 'Skövde')
- INSERT INTO GruppenTbl VALUES 
- (3, 'Jesper', 'Whendin', 29, 'Kungsbacka')
-  INSERT INTO GruppenTbl VALUES 
- (4, 'Joseph', 'Rashid-Maalouf', 30, 'Göteborg')
-  INSERT INTO GruppenTbl VALUES 
-  (5, 'Mikael', 'Björnänger', 35, 'Göteborg');
+--INSERT INTO GruppenTbl VALUES
+-- (1, 'Tyr', 'Hvidfeldt', 37, 'Göteborg')
+-- INSERT INTO GruppenTbl VALUES 
+-- (2, 'Gabriel', 'Raimondo', 31, 'Skövde')
+-- INSERT INTO GruppenTbl VALUES 
+-- (3, 'Jesper', 'Whendin', 29, 'Kungsbacka')
+--  INSERT INTO GruppenTbl VALUES 
+-- (4, 'Joseph', 'Rashid-Maalouf', 30, 'Göteborg')
+--  INSERT INTO GruppenTbl VALUES 
+--  (5, 'Mikael', 'Björnänger', 35, 'Göteborg');
 
 
 
